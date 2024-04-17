4114564762cb729cd1bc3b60c9037e193182a51e
Yusuf Simsek <yusuftariksimsek@gmail.com>
2024-04-17 14:42:42 -0400

    Tests

--- HW5/src/Examples.java
+++ HW5/src/Examples.java
@@ -16,4 +16,80 @@ public class Examples {
             20240405.0 ,75.0,7.0,76.0,11.0,81.0,15.0,65.0,12.0,-999.0,75.0,10.0,
             20240406.0 ,75.0,6.0,76.0,11.0,82.0,15.0,65.0,12.0,-999.0,75.0,10.0);
 
+    @Test
+    public void typicalGreenHouse(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 1));
+        gh.intakeData(testData);
+        assertEquals(Optional.of(80.0), gh.maxTemperature(20240404.0));
+    }
+    @Test
+    public void ignoreDataForDateGreenHouse(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 7));
+        gh.intakeData(testData);
+        assertEquals(Optional.empty(), gh.maxTemperature(20240404));
+    }
+    @Test
+    public void encapsulateGCTestGreenHouse(){
+        GregorianCalendar gc = new GregorianCalendar(2024, Calendar.APRIL, 6);
+        GreenHouse gh = new GreenHouse(gc);
+        gc.set(Calendar.DAY_OF_MONTH, 1);
+        gh.intakeData(testData);
+        assertEquals(Optional.of(82.0), gh.maxTemperature(20240406));
+    }
+
+    @Test
+    public void typicalGreenHouse2(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 1));
+        gh.intakeData(testData);
+        assertEquals(Optional.of(8.0), gh.minHumidity(20240404.0));
+    }
+    @Test
+    public void ignoreDataForDateGreenHouse2(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 7));
+        gh.intakeData(testData);
+        assertEquals(Optional.empty(), gh.minHumidity(20240404));
+    }
+    @Test
+    public void encapsulateGCTestGreenHouse2(){
+        GregorianCalendar gc = new GregorianCalendar(2024, Calendar.APRIL, 6);
+        GreenHouse gh = new GreenHouse(gc);
+        gc.set(Calendar.DAY_OF_MONTH, 1);
+        gh.intakeData(testData);
+        assertEquals(Optional.of(6.0), gh.minHumidity(20240406));
+    }
+
+    @Test
+    public void typicalGreenHouseNoDate(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 1));
+        gh.intakeData(testData);
+        assertEquals(Optional.of(82.0), gh.maxTemperature());
+    }
+    @Test
+    public void ignoreDataForDateGreenHouseNoDate(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 7));
+        gh.intakeData(testData);
+        assertEquals(Optional.empty(), gh.maxTemperature());
+    }
+
+    @Test
+    public void typicalGreenHouseNoDate2(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 1));
+        gh.intakeData(testData);
+        assertEquals(Optional.of(6.0), gh.minHumidity());
+    }
+    @Test
+    public void ignoreDataForDateGreenHouseNoDate2(){
+        GreenHouse gh = new GreenHouse(new GregorianCalendar(2024, Calendar.APRIL, 7));
+        gh.intakeData(testData);
+        assertEquals(Optional.empty(), gh.minHumidity());
+    }
+
+    @Test
+    public void encapsulateGCTestGreenHouse3(){
+        GregorianCalendar gc = new GregorianCalendar(2024, Calendar.APRIL, 6);
+        GreenHouse gh = new GreenHouse(gc);
+        gc.set(Calendar.DAY_OF_MONTH, 1);
+        gh.intakeData(testData);
+        assertEquals(Optional.empty(), gh.maxTemperature(20240403));
+    }
 }
--- HW5/src/GreenHouse.java
+++ HW5/src/GreenHouse.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class GreenHouse {
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Optional;
+
+public class GreenHouse implements IGreenHouse {
+    public GreenHouse(GregorianCalendar cal){
+
+    }
+    @Override
+    public void intakeData(List<Double> data) {
+
+    }
+
+    @Override
+    public Optional<Double> maxTemperature(double forDate) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Double> maxTemperature() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Double> minHumidity(double forDate) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Double> minHumidity() {
+        return Optional.empty();
+    }
+
+    @Override
+    public void setStrategy(IGHStrategy strategy) {
+
+    }
 }
